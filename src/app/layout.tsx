b3532e69f1a3c04a3051a8089c1fe274785ebc24
Ibrahim Abdallah <hemade@gmail.com>
2025-09-12 22:39:07 +0300

    #

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// "use client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { store } from "../store";
@@ -29,7 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider store={store}>{children}</Provider>
+        {/* <Provider store={store}>{children}</Provider> */}
+        {children}
       </body>
     </html>
   );
