2c89f87f01b488e95fc363542bfd0e4fb2b7b1e0
Ibrahim Abdallah <hemade@gmail.com>
2025-09-17 23:28:59 +0300

    #

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,5 @@
-import StudentsSkeleton from "@/components/Skeleton/Skeleton";
 import React from "react";
 
 export default function loading() {
-  return <StudentsSkeleton />;
+  return <div>Loading... </div>;
 }
