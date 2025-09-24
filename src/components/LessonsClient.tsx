2a076edb36f9d4eba723e6a0cac9b5d7bd72394b
Ibrahim Abdallah <hemade@gmail.com>
2025-09-24 13:52:11 +0300

    change type for lesson

--- src/components/LessonsClient.tsx
+++ src/components/LessonsClient.tsx
@@ -11,7 +11,7 @@ const LessonsClient = () => {
   const { lessons, loading, error } = useAppSelector((state) => state.lessons); // Access lessons state
   const { token } = useAppSelector((state) => state.auth); // Access lessons state
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
   const [allLessons, setAllLessons] = useState<typeof lessons>([]);
   // console.log('lessons' , lessons);
   useEffect(() => {
@@ -23,9 +23,10 @@ const LessonsClient = () => {
     loadLessons();
   }, [dispatch, token]);
 
-
-  const filteredLessons = allLessons.filter((lesson) => //filter lessons based on title
-    lesson.title.toLowerCase().includes(search.toLowerCase())
+  const filteredLessons = allLessons.filter(
+    (
+      lesson: Lesson //filter lessons based on title
+    ) => lesson.title.toLowerCase().includes(search.toLowerCase())
   );
   if (loading) return <p className="text-center py-8">Loading lessons...</p>;
   if (error) return <p className="text-center py-8 text-red-500">{error}</p>;
@@ -40,23 +41,22 @@ const LessonsClient = () => {
           type="text"
           placeholder="Search by title"
           value={search}
-
-          onChange={(e) => setSearch(e.target.value)}  //update search state on input change
+          onChange={(e) => setSearch(e.target.value)} //update search state on input change
           className="flex-1  p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
         />
       </div>
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {filteredLessons.length > 0 ?
-          (filteredLessons.map((lesson) => (
+        {filteredLessons.length > 0 ? (
+          filteredLessons.map((lesson: Lesson) => (
             <LessonCard key={lesson._id} lesson={lesson} />
-          ))) : (
-            <p className="text-center text-gray-500 col-span-full">
-              {search.trim().length > 0
-                ? `No lessons found for "${search}"`
-                : "No lessons available."}
-            </p>
-          )}
-
+          ))
+        ) : (
+          <p className="text-center text-gray-500 col-span-full">
+            {search.trim().length > 0
+              ? `No lessons found for "${search}"`
+              : "No lessons available."}
+          </p>
+        )}
       </div>
     </div>
   );
